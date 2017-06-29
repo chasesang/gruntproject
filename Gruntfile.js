6e6afd25948b4d7ab775769062601f81ff6073af
Chase Sang <chase.sang@gmail.com>
2017-06-29 15:47:38 -0700

    2nd commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,27 @@
 module.exports = function(grunt) {
-  grunt.registerTask('speak', function(){
-    console.log("i am speaking");
+  // grunt.registerTask('speak', function(){
+  //   console.log("i am speaking");
+  //
+  // });
+  //
+  // grunt.registerTask('yell', function(){
+  //   console.log("i am yelling");
+  //
+  // });
+  //
+  // grunt.registerTask('default',['speak', 'yell']);
 
-  });
 
-  grunt.registerTask('yell', function(){
-    console.log("i am yelling");
+  // Project configuration.
+grunt.initConfig({
+  concat: {
 
-  });
-
-  grunt.registerTask('default',['speak', 'yell']);
+    dist: {
+      src: ['src/intro.js', 'src/project.js', 'src/outro.js'],
+      dest: 'dist/built.js',
+    },
+  },
+});
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
 }
